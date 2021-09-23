00964d1bac0c4566a4e4c6a902f7e44b45fd150a
gfm <gfmwebpost@gmail.com>
2021-09-23 11:47:41 +0500

    getUserTrainingByID добавлен

--- api/user.php
+++ api/user.php
@@ -97,6 +97,67 @@ class user
 						'DATA'=>$response
 					]);
 			}
+            elseif ($operation == 'getUserTrainingByID'){ // Забрать тренировки пользователя
+
+	            if(!isset($params[0][2])){
+		            new ResponseController([
+			            'format'=>'JSON',
+			            'DATA'=>['status'=>404,'message'=>'После указания метода ожидается ID тренировки']
+		            ]);
+	            }
+
+	            $count = rand(8,24); // Количество Элементов тренировок в массиве ответов
+	            $treners    =   ['trener1.jpg','trener3.jpeg','trener5.jpg'];
+	            $Titles     =   ['Йога','Силовая','Кардио','Другая'];
+	            $TrainType  =   ['Индивидуальная','Групповая'];
+	            $days       =   ['Завтра','16.09','18.09','19.09','20.09','21.09','22.09','23.09','24.09','25.09','26.09','27.09','28.09','29.09'];
+	            $times      =   ['12:00','14:30','15:30','18:20'];
+	            $rooms      =   ['Основной зал', 'Левый зал', 'Правый зал'];
+	            $price      =   [0,900,1200,300,500];
+	            $discount   =   [0,2,5,10];
+	            $element    =   [ // Массив одной тренировки
+		            'TrainingId'=>$params[0][2],
+		            'Img'=>'content/upload/treners/'.$treners[2],
+		            'Title'=>$Titles[2],
+		            'Type'=>$TrainType[1],
+		            'Remain'=>rand(1,7),
+		            'Next'=>$days[3],
+		            'Action'=>'showTraningInfo',
+		            'Target'=>$token,
+		            'Time'=>$times[rand(0,3)],
+		            'Date'=>$days[3],
+		            'IsRecommended'=>0,
+		            'IsNew'=>0,
+		            'Room'=>$rooms[1],
+		            'IsPaid'=>1,
+		            'Price'=>$price[3],
+		            'Discount'=>$discount[0],
+		            'BalanceOnShop'=>4,
+		            'CategoryId'=>1,
+		            'CategoryName'=>$Titles[2],
+		            'HasMaster'=>1,
+		            'Masters'=>[
+			            'MasterId'=>10,
+			            'MasterFirstName'=>'Имя',
+			            'MasterLastName'=>'Фамилия',
+			            'MasterPatronymic'=>'Отчество'
+		            ],
+
+	            ];
+	            $ResponceBody = [];
+	            for ($i=0; $i <1; $i++){
+		            array_push($ResponceBody,$element);
+	            }
+	            $response =	[
+		            'status'=>200,
+		            'message'=>'Ok',
+		            'UserTrainings'=>$ResponceBody
+	            ];
+	            new ResponseController([
+		            'format'=>'JSON',
+		            'DATA'=>$response
+	            ]);
+            }
         }
         new ResponseController([
            'format'=>'JSON',
